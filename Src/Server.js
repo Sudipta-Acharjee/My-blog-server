645a581f493bc91d084ec32fbfb5e9f3c278ae69
unknown <sudiptoacharjee4@gmail.com>
2021-10-02 09:07:03 +0600

    updated

--- Src/Server.js
+++ Src/Server.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => res.send("Hello"));
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
+app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
