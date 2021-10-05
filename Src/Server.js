38472c94dfa02d8eeddcb9fc20d1cc2bba5ff68a
unknown <sudiptoacharjee4@gmail.com>
2021-10-05 15:15:21 +0600

    server get

--- Src/Server.js
+++ Src/Server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const {MongoClient} = require("mongodb");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get('/api/articles/:name', (req, res) => {
+    const articleName = req.params.name;
+    const client = MongoClient.connect('mongodb://localhost:27017',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    const db= client.db("myblog")
+    const articleInfo=db.collection('articles').findOne({name: articleName});
+    res.status(200).json(articleInfo);
+    client.close();
+});
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
     const { username, text } = req.body;
